27594c47121a6ddbb16d2ea9b1088d9b38f32d40
mamangs <mamangndi69@gmail.com>
2021-02-11 14:22:28 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,88 @@
+<?php 
+
+/*
+Eldi Anugrah Pratama
+203040031
+https://github.com/mamang69/pw2021_203040031
+Pertemuan2 - 11 Februari 2021
+mempelajari sintak PHP
+*/
+
+?>
+
+
+
+<?PHP 
+// Pertemuan 2- PHP Dasar
+// sintaks PHP
+
+// standar output
+// echo,print
+// print_r
+// var_dump
+
+
+// penulisan sintak php
+// 1.PHP dalam HTMl
+// 2.HTML dalam PHP
+
+
+
+// variable dan tipe data
+// variable
+// nama tidak boleh diawali anggak namun boleh mengandung angka
+
+// $nama="Mamang Ndi";
+// echo 'halo nama saya $nama';
+
+
+
+// operator
+// 1.aritmatika
+// + - * / %
+
+// $x=10;
+// $y=3;
+// echo $x+$x;
+
+
+// penggabung string /contatenation/concat
+// .
+// $nama_depan = "Mamang";
+// $nama_belakang= "Eldi";
+// echo $nama_depan ." " . $nama_belakang;
+
+
+// asigment
+// =, +=, -=, /=, %=, .=
+// $x =5;
+// $x +=7;
+// echo $x;
+// $nama ="Mamang";
+// $nama .= "   ";
+// $nama .="Eldi";
+
+// echo $nama;
+
+
+
+// operator perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == 1);
+
+
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+
+
+// logika
+// &&, ||, !
+// $y=50;
+
+// var_dump($y <10 && $y %5 ==0);
+
+?>
+
