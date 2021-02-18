ba4c40e37fcfd9f8eaa4154ce003569a811635b9
mamangs <mamangndi69@gmail.com>
2021-02-18 23:15:11 +0700

    menyelesaikan video pertemuan ke4

---
+++ pertemuan4/date.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Eldi Anugrah Pratama
+203040031
+https://github.com/mamang69/pw2021_203040031
+Pertemuan4 -  18 Februari 2021
+mempelajari pengulangan PHP
+*/
+?>
+<?php
+//date
+//untuk menampikan tanggal dengan format tertentu
+// echo date("l,d-M-y");
+
+//time
+//UNIX Timestamp/ EPOCH time
+//detik yang sudah berlalu sejak 1 januari 1970
+// echo time();
+// echo date("d M y",time()-60*60*24*100);
+
+//mktime
+//membuat detik sendiri
+//mktime(0,0,0,0,0,0)
+//jam, menit, detik, bulan, tanggal, tahun
+// echo date( "l",mktime(0,0,0,9,28,2002));
+
+//strtotime
+// echo date ("l",strtotime("28 september 2002"));
+?>
