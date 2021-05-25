7a0da147f88442e246b6de0b96fa64eb4082aa8c
mamangs <mamangndi69@gmail.com>
2021-05-25 20:47:41 +0700

    menyelesaikan tubes

---
+++ index/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mamang's</title>
+  <link rel="icon" href="img/palet.png">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="asset/fonts/Product_Sans_Regular.ttf">
+
+
+
+</head>
+
+<body>
+
+  <div class="wrapper">
+    <div class="nav">
+      <div class="logo">
+        <h4>Mamang'shOp</h4>
+      </div>
+      <div class="links">
+        <a href="#" class="mainlink">home</a>
+        <a href="#about">about</a>
+        <a href="#p">project Semester 2</a>
+
+      </div>
+    </div>
+
+    <!---landing page-->
+    <div class="landing">
+      <div class="landingteks">
+        <h1> <span class="efek-ngetik"></span> semuanya <span style="color: #38d39f ;">Welcome To ma
+            web</span> </h1>
+        <h3>i Hope you guys enjoy ma web <br> please coment for ma web:)</h3>
+        <div class="btn">
+          <a href="#">learn more</a>
+        </div>
+      </div>
+      <div class="landingimage">
+        <img src="img/palet.png" alt="">
+      </div>
+    </div>
+
+
+    <!---about section-->
+    <div class="about" id="about">
+      <div class="aboutteks">
+        <h1>
+          Biodata tentang <br> <span style="color: #38d39f; font-size: 3vw;"> saya </span></h1>
+        <img src="img/mangs.png" alt="">
+      </div>
+      <div class="aboutlist">
+        <ol>
+          <li><span>></span>
+            <p>Nama saya Eldi Anugrah Pratama saya lahir du Sukabumi 28 September 2002.
+              Hobi saya sebagai nolep adalah nyoba-nyoba coding ,ngedit foto atau video, nonton anime, baca manga,main
+              game,
+              dan nonton game. Cita-cita saya menjadi seorang full stack develover dan pengusaha.
+              doakan saya teman teman supaya cita cita saya terwujud dan membanggakan ortu saya. </p>
+          </li>
+
+        </ol>
+      </div>
+    </div>
+
+
+    <!--info section-->
+
+    <div class="infosection">
+      <div class="infoheader">
+        <h1 class="p" id="p">salah satu hasil dari <br> <span style="color: #38d39f;">hobi saya ini.</span> </h1>
+      </div>
+      <div class="infocards">
+        <div class="card one">
+          <img src="img/kuliah.png" class="cardoneimg" alt="">
+          <div class="cardbgone"></div>
+          <div class="cardcontent">
+            <h2>Kuliah</h2>
+            <p>kumpulan kodingan/program yang bertujuan untuk memenuhi tugas yang diberikan oleh dosen</p>
+            <a href="../kuliah/">
+              <div class="cardbtn">
+                <p>>></p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="card two">
+          <img src="img/praktikum.png" class="cardtwoimg" alt="">
+          <div class="cardbgtwo"></div>
+          <div class="cardcontent">
+            <h2>Praktikum</h2>
+            <p>kumpulan kodingan/program yang bertujuan untuk memenuhi tugas yang diberikan oleh para asisten dosen</p>
+            <a href="../praktikum/">
+              <div class="cardbtn">
+                <p>>></p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="card three">
+          <img src="img/tubes.png" class="cardthreeimg" alt="">
+          <div class="cardbgthree"></div>
+          <div class="cardcontent">
+            <h2>Tugas besar</h2>
+            <p>salah satu editan ketika gabut dan kebetulan dapat inspirasi dari Nartoh ketika madara menggunakan mugen
+              tsukuyomi</p>
+            <a href="../tubes/">
+              <div class="cardbtn">
+                <p>>></p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+
+  </div>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
